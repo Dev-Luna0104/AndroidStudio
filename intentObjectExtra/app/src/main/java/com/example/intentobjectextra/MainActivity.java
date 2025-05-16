3d8bf93283b5d176efe936f0c6594a8ad9d8e7bf
Lab05_etec <Lab05_etec@etec>
2025-05-15 21:21:49 -0300

    intent_002

--- intentObjectExtra/app/src/main/java/com/example/intentobjectextra/MainActivity.java
+++ intentObjectExtra/app/src/main/java/com/example/intentobjectextra/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.intentobjectextra;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 
 public class MainActivity extends Activity {
 
     Button btnSalvarCadastro;
+    EditText edtNome, edtIdade;
     Spinner spnCurso;
-    String curso;
+    RadioGroup rgGenero, rgStatusMatricula;
+    RadioButton rbMasculino, rbFeminino, rbEmCurso, rbTrancado;
+    String nome, curso, genero, statusMatricula;
+    int idade;
     Intent intent;
-    Bundle bundle;
+    Pessoa aluno;
 
 
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +34,39 @@ public class MainActivity extends Activity {
 
         btnSalvarCadastro = findViewById(R.id.btnSalvarCadastro);
 
+        edtNome = findViewById(R.id.txtNome);
+        edtIdade = findViewById(R.id.txtIdade);
         spnCurso = findViewById(R.id.spnCurso);
+        rgGenero = findViewById(R.id.rdgGenero);
+        rgStatusMatricula = findViewById(R.id.chbStatusMatricula);
+        rbMasculino = findViewById(R.id.rdbMasculino);
+        rbFeminino = findViewById(R.id.rdbFeminino);
 
         configuraSpinner();
 
+        btnSalvarCadastro.setOnClickListener(v -> {
+            nome = edtNome.getText().toString();
+            idade = Integer.parseInt(edtIdade.getText().toString());
+            curso = spnCurso.getSelectedItem().toString();
+
+            if (rbMasculino.isChecked()){
+                genero = "Masculino";
+            } else if (rbFeminino.isChecked()) {
+                genero = "Feminino";
+            }
+
+            if(rbEmCurso.isChecked()){
+                statusMatricula = "Em curso";
+            } else if (rbTrancado.isChecked()) {
+                statusMatricula = "Trancado";
+            }
+
+            aluno = new Pessoa(nome, idade, curso, genero, statusMatricula);
+            intent = new Intent(MainActivity.this, DetalheActivity.class);
+            intent.putExtra("aluno", aluno);
+
+            startActivity(intent);
+        });
 
     }
 
@@ -36,7 +74,7 @@ public class MainActivity extends Activity {
         String[] cursos = new String[]{"Informática", "Mecânica", "Eletrônica"};
 
         //criar um adaptador para o spinner
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cursos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, cursos);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //associar o adaptador ao spinner
         spnCurso.setAdapter(adapter);
--- intentObjectExtra/app/src/main/java/com/example/intentobjectextra/Pessoa.java
+++ intentObjectExtra/app/src/main/java/com/example/intentobjectextra/Pessoa.java
@@ -2,23 +2,46 @@ package com.example.intentobjectextra;
 
 import java.io.Serializable;
 
-//Value Object (VO)
+//value
 public class Pessoa implements Serializable {
 
     private String nome;
     private int idade;
+    private String curso;
+    private String genero;
+    private String statusMatricula;
 
-    public Pessoa(String nome, int idade) {
+    public Pessoa(String nome, int idade, String curso, String genero, String statusMatricula) {
         this.nome = nome;
         this.idade = idade;
+        this.curso = curso;
+        this.genero = genero;
+        this.nome = statusMatricula;
+
     }
 
-    public String getNome() {
-        return nome;
+    public String getStatusMatricula() {
+        return statusMatricula;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setStatusMatricula(String statusMatricula) {
+        this.statusMatricula = statusMatricula;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
     }
 
     public int getIdade() {
@@ -29,8 +52,11 @@ public class Pessoa implements Serializable {
         this.idade = idade;
     }
 
+    public String getNome() {
+        return nome;
+    }
 
-
-
-
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
